0e6621ce29a6fa215e3eb8bd06d5e7bbb996abaa
Maria Minor <meverik88tt@gmail.com>
2022-06-08 22:18:45 +0300

    branch coverage 100 percent

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,6 +3,57 @@ package ru.netology.domain;
 
 public class Radio {
 
-    public int currentRadioStationNumber;
-    public int soundVolume;
+    private int currentRadioStationNumber = 0;
+    private int currentSoundVolume = 5;
+
+
+    public void setCurrentRadioStationNumber(int stationNumber) {
+        if (stationNumber >= 0 && stationNumber <= 9) {
+            this.currentRadioStationNumber = stationNumber;
+        }
+
+    }
+
+    public void nextStation() {
+        if (this.currentRadioStationNumber < 9) {
+            this.currentRadioStationNumber = this.currentRadioStationNumber + 1;
+        } else {
+            this.currentRadioStationNumber = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (this.currentRadioStationNumber > 0) {
+            this.currentRadioStationNumber = this.currentRadioStationNumber - 1;
+        } else {
+            this.currentRadioStationNumber = 9;
+        }
+    }
+
+    public int getCurrentRadioStationNumber() {
+        return this.currentRadioStationNumber;
+
+    }
+
+    public int getCurrentSoundVolume() {
+        return this.currentSoundVolume;
+
+    }
+
+
+    public void increaseVolume() {
+        if (this.currentSoundVolume < 10) {
+            this.currentSoundVolume = this.currentSoundVolume + 1;
+        }
+
+    }
+
+    public void decreaseVolume() {
+        if (this.currentSoundVolume > 0) {
+            this.currentSoundVolume = this.currentSoundVolume - 1;
+        }
+
+    }
+}
+
 
--- src/test/java/ru/netology/domain/RadioServiceTest.java
+++ src/test/java/ru/netology/domain/RadioServiceTest.java
@@ -1,5 +1,6 @@
 package ru.netology.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,15 +8,159 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RadioServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxNumber() {
         Radio cond = new Radio();
+
+        cond.setCurrentRadioStationNumber(6);
+
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void findMaxNumberAbove() {
+        Radio cond = new Radio();
+
         cond.setCurrentRadioStationNumber(10);
 
-        int actual = cond.getCurrentRadioStationNumber();
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void findMaxNumberBelow() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadioStationNumber(-2);
+
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextStationShouldSetNextCurrentRadioStationNumberIfAboveNine() {
+        Radio cond = new Radio();
+
+        cond.nextStation();
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextStationShouldSetNextCurrentRadioStationNumberIfStationIsNine() {
+        Radio cond = new Radio();
+        cond.setCurrentRadioStationNumber(9);
+
+        cond.nextStation();
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void prevStationShouldSetNextCurrentRadioStationNumberIfAboveZero() {
+        Radio cond = new Radio();
+
+        cond.prevStation();
+
+
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
         int expected = 9;
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevStationShouldSetNextCurrentRadioStationNumberIfStationIsNine() {
+        Radio cond = new Radio();
+
+        cond.setCurrentRadioStationNumber(9);
+
+        cond.prevStation();
 
 
+        int actual = cond.getCurrentRadioStationNumber();//cond.currentRadioStationNumber;;
+        int expected = 8;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseSoundVolume() {
+        Radio cond = new Radio();
+
+        cond.increaseVolume();
+
+
+        int actual = cond.getCurrentSoundVolume();//cond.currentRadioStationNumber;;
+        int expected = 6;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void increaseSoundVolumeFalse() {
+        Radio cond = new Radio();
+
+        cond.increaseVolume();
+        cond.increaseVolume();
+        cond.increaseVolume();
+        cond.increaseVolume();
+        cond.increaseVolume();
+        cond.increaseVolume();
+
+        int actual = cond.getCurrentSoundVolume();//cond.currentRadioStationNumber;;
+        int expected = 10;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void decreaseSoundVolume() {
+        Radio cond = new Radio();
+
+        cond.decreaseVolume();
+
+
+        int actual = cond.getCurrentSoundVolume();//cond.currentRadioStationNumber;;
+        int expected = 4;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void decreaseSoundVolumeFalse() {
+        Radio cond = new Radio();
+
+        cond.decreaseVolume();
+        cond.decreaseVolume();
+        cond.decreaseVolume();
+        cond.decreaseVolume();
+        cond.decreaseVolume();
+        cond.decreaseVolume();
+
+        int actual = cond.getCurrentSoundVolume();//cond.currentRadioStationNumber;;
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 }
+
+
