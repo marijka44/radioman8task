9ddbaab7fa975fa40aa66ed18184e6aa673b944b
Maria Minor <meverik88tt@gmail.com>
2022-06-15 12:25:42 +0300

    radioman upgrade

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,59 +1,67 @@
 package ru.netology.domain;
 
-
 public class Radio {
+    private final int countStationNumber;
+
+    private final int firstStationNumber = 0;
 
     private int currentRadioStationNumber = 0;
+
+    private final int minSoundVolume = 0;
+
+    private final int maxSoundVolume = 100;
     private int currentSoundVolume = 5;
 
-
-    public void setCurrentRadioStationNumber(int stationNumber) {
-        if (stationNumber >= 0 && stationNumber <= 9) {
-            this.currentRadioStationNumber = stationNumber;
-        }
-
+    public Radio() {
+        this.countStationNumber = 10;
     }
 
-    public void nextStation() {
-        if (this.currentRadioStationNumber < 9) {
-            this.currentRadioStationNumber = this.currentRadioStationNumber + 1;
-        } else {
-            this.currentRadioStationNumber = 0;
-        }
-    }
-
-    public void prevStation() {
-        if (this.currentRadioStationNumber > 0) {
-            this.currentRadioStationNumber = this.currentRadioStationNumber - 1;
-        } else {
-            this.currentRadioStationNumber = 9;
-        }
+    public Radio(int countStationNumber) {
+        this.countStationNumber = countStationNumber;
     }
 
     public int getCurrentRadioStationNumber() {
         return this.currentRadioStationNumber;
-
     }
 
     public int getCurrentSoundVolume() {
         return this.currentSoundVolume;
-
     }
 
+    public void setCurrentRadioStationNumber(int stationNumber) {
+        if (stationNumber >= this.firstStationNumber && stationNumber < this.countStationNumber) {
+            this.currentRadioStationNumber = stationNumber;
+        }
+    }
+
+    public void nextStation() {
+        if (this.currentRadioStationNumber < this.countStationNumber - 1) {
+            this.currentRadioStationNumber = this.currentRadioStationNumber + 1;
+        } else {
+            this.currentRadioStationNumber = this.firstStationNumber;
+        }
+    }
+
+    public void prevStation() {
+        if (this.currentRadioStationNumber > this.firstStationNumber) {
+            this.currentRadioStationNumber = this.currentRadioStationNumber - 1;
+        } else {
+            this.currentRadioStationNumber = this.countStationNumber - 1;
+        }
+    }
 
     public void increaseVolume() {
-        if (this.currentSoundVolume < 10) {
+        if (this.currentSoundVolume < this.maxSoundVolume) {
             this.currentSoundVolume = this.currentSoundVolume + 1;
         }
-
     }
 
     public void decreaseVolume() {
-        if (this.currentSoundVolume > 0) {
+        if (this.currentSoundVolume > this.minSoundVolume) {
             this.currentSoundVolume = this.currentSoundVolume - 1;
         }
-
     }
+
 }
 
 
